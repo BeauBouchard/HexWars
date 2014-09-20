92f4641f22ee4afa9aa37f568b542f46eeefbc3d
Beau Bouchard <bnb2011@gmail.com>
2014-09-20 15:05:06 -0400

    Update game.js

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -4,11 +4,18 @@
  */
 
 var game = function () {
-	this.gamewindow = document.getElementById("game");
-		this.canvas 		= document.createElement("canvas");
-		this.context 		= this.canvas.getContext("2d");
-		this.canvas.width 	= 512;
-		this.canvas.height	= 256;
-				this.gamewindow.appendChild(this.canvas);
+
 }
+
+
+game.prototype.start = function(gamestate, callback) {
+		
+}
+
+game.prototype.init = function() {
+	this.gamestate = 0; 
+}
+
+
+
 
