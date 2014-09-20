c3b431a37b38ace7b4e14a269f73d6c316c9311f
Beau Bouchard <bnb2011@gmail.com>
2014-09-20 15:27:38 -0400

    Update map.js

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -6,32 +6,32 @@ var map = function () {
   	this.context 		= this.canvas.getContext("2d");
   	this.canvas.width 	= 512;
   	this.canvas.height	= 256;
+  	
+  	this.tileset = []; 
 
   this.gamewindow.appendChild(this.canvas);
     this.context.fillStyle = "#000000";
-    this.context.strokeStyle = "#CCCCCC";
+    this.context.strokeStyle = "#FFFFFF";
     this.context.lineWidth = 1;
     
-    drawBoard(ctx, boardWidth, boardHeight);
+    initGrid(this.context, this.canvas.width, this.canvas.height);
 }
 
-map.prototype.drawBoard = function (canvasContext, width, height) {
-        var i,
-            j;
+map.prototype.initGrid = function (canvasContext, width, height) {
+        var i, j; // counting variables
  
         for(i = 0; i < width; ++i) {
             for(j = 0; j < height; ++j) {
-                drawTile(
-                    map.context, 
+            	var atile = new Tile(canvasContext, 
                     i * hexRectangleWidth + ((j % 2) * hexRadius), 
                     j * (sideLength + hexHeight), 
-                    false
-                );
+                    false);
+                this.tileset[] += atile; 
             }
         }
 }
 
-var tile = function () {
+var Tile = function (canvasContext, x, y, fill) {
   var hexHeight,
     hexRadius,
     hexRectangleHeight,
@@ -45,9 +45,11 @@ var tile = function () {
     hexRadius = Math.cos(hexagonAngle) * sideLength;
     hexRectangleHeight = sideLength + 2 * hexHeight;
     hexRectangleWidth = 2 * hexRadius;
+    
+    this.drawTile(canvasContext, x, y, fill);
 }
 
-tile.prototype.drawTile = function(canvasContext, x, y, fill) { 
+Tile.prototype.drawTile = function(canvasContext, x, y, fill) { 
         var fill = fill || false;
  
         canvasContext.beginPath();
