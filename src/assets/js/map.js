014a78d8dbdab374fd7de7791a9d4e2d4660e789
Beau Bouchard <bnb2011@gmail.com>
2014-09-20 15:51:14 -0400

    Update map.js

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,71 +1,77 @@
+
 	
-	
-var map = function () {
+function MAP() {
 	this.gamewindow = document.getElementById("game");
   	this.canvas 		= document.createElement("canvas");
   	this.context 		= this.canvas.getContext("2d");
-  	this.canvas.width 	= 512;
-  	this.canvas.height	= 256;
+  	this.canvas.width 	= 1024;
+  	this.canvas.height	= 762;
   	
   	this.tileset = []; 
 
   this.gamewindow.appendChild(this.canvas);
-    this.context.fillStyle = "#000000";
+    this.context.fillStyle = "#212121";
     this.context.strokeStyle = "#FFFFFF";
     this.context.lineWidth = 1;
     
-    initGrid(this.context, this.canvas.width, this.canvas.height);
+    this.initGrid(this.context, this.canvas.width, this.canvas.height);
 }
 
-map.prototype.initGrid = function (canvasContext, width, height) {
+MAP.prototype = {
+    initialize: function() {
+
+    }
+}
+MAP.prototype.initGrid = function (canvasContext, width, height) {
         var i, j; // counting variables
  
         for(i = 0; i < width; ++i) {
             for(j = 0; j < height; ++j) {
-            	var atile = new Tile(canvasContext, 
-                    i * hexRectangleWidth + ((j % 2) * hexRadius), 
-                    j * (sideLength + hexHeight), 
-                    false);
-                this.tileset[] += atile; 
+            	var atile = new TILE();
+                //    this.drawTile(canvasContext, x, y, fill);
+                    atile.drawTile(canvasContext, 
+                    i * atile.hexRectangleWidth + ((j % 2) * atile.hexRadius), 
+                    j * (atile.sideLength + atile.hexHeight), 
+                    true);
+                this.tileset.push(atile); 
             }
         }
 }
 
-var Tile = function (canvasContext, x, y, fill) {
-  var hexHeight,
-    hexRadius,
-    hexRectangleHeight,
-    hexRectangleWidth,
-    hexagonAngle = 0.523598776, // 30 degrees in radians
-    sideLength = 36,
-    boardWidth = 10,
-    boardHeight = 10;
+var TILE = function (canvasContext, x, y, fill) {
+  this.hexHeight,
+    this.hexRadius,
+    this.hexRectangleHeight,
+    this.hexRectangleWidth,
+    this.hexagonAngle = 0.523598776, // 30 degrees in radians
+    this.sideLength = 36,
+    this.boardWidth = 10,
+    this.boardHeight = 10;
     
-    hexHeight = Math.sin(hexagonAngle) * sideLength;
-    hexRadius = Math.cos(hexagonAngle) * sideLength;
-    hexRectangleHeight = sideLength + 2 * hexHeight;
-    hexRectangleWidth = 2 * hexRadius;
+    this.hexHeight = Math.sin(this.hexagonAngle) * this.sideLength;
+    this.hexRadius = Math.cos(this.hexagonAngle) * this.sideLength;
+    this.hexRectangleHeight = this.sideLength + 2 * this.hexHeight;
+    this.hexRectangleWidth = 2 * this.hexRadius;
     
-    this.drawTile(canvasContext, x, y, fill);
+
 }
 
-Tile.prototype.drawTile = function(canvasContext, x, y, fill) { 
-        var fill = fill || false;
- 
-        canvasContext.beginPath();
-        canvasContext.moveTo(x + hexRadius, y);
-        canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight);
-        canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight + sideLength);
-        canvasContext.lineTo(x + hexRadius, y + hexRectangleHeight);
-        canvasContext.lineTo(x, y + sideLength + hexHeight);
-        canvasContext.lineTo(x, y + hexHeight);
-        canvasContext.closePath();
- 
-        if(fill) {
-            canvasContext.fill();
-        } else {
-            canvasContext.stroke();
-        }
+TILE.prototype.drawTile = function(canvasContext, x, y, fill) { 
+    var fill = fill || false;
+
+    canvasContext.beginPath();
+    canvasContext.moveTo(x + this.hexRadius, y);
+    canvasContext.lineTo(x + this.hexRectangleWidth, y + this.hexHeight);
+    canvasContext.lineTo(x + this.hexRectangleWidth, y + this.hexHeight + this.sideLength);
+    canvasContext.lineTo(x + this.hexRadius, y + this.hexRectangleHeight);
+    canvasContext.lineTo(x, y + this.sideLength + this.hexHeight);
+    canvasContext.lineTo(x, y + this.hexHeight);
+    canvasContext.closePath();
+
+    if(fill) {
+        canvasContext.fill();
+    } else {
+        canvasContext.stroke();
     }
 }
 
