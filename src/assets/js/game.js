b7fd46297f703dbee0f715c4d35a5ba1a011147a
Beau Bouchard <bnb2011@gmail.com>
2014-09-20 20:30:22 -0400

    Update game.js

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -3,34 +3,81 @@
  *  Houses game logic for HexWars
  */
 
-var map;
+
 
 function GAME() {
 	this.gamestate = 0; 
+	this.map;
+	this.players = [];
 }
 GAME.prototype = {
     initialize: function() {
-
+    	console.log("Initializing Game");
+    	this.map = new MAP();
+    	this.map.initialize();
     },
     start: function() {
-
+    	console.log("Starting Game");
+    	this.setupPlayers(2);
     },
     getGameState: function() {
     	return this.gamestate;
     },
     restart: function() {
 
+    },
+    setupPlayers: function (numPlayers) {
+    	console.log("Setting up game with "+numPlayers+" Players");
+    	for(var i = 1; i <= numPlayers; i++){
+    		var player = new PLAYER(i);
+    		this.players.push(player);
+    	}
+    	this.setPlayerColor();
+    },
+    setPlayerColor: function() {
+    	//console.log(this.players.pop().getColor());
+    	this.map.setTileColor(1,1,this.players[0].getColor());
     }
 }
 
-function PLAYER() {
-
+function PLAYER(playernum) {
+	this.number = playernum;
+	this.team = new TEAM(this.number);
 }
 
 PLAYER.prototype = {
     initialize: function() {
 
+    },
+    getColor: function() {
+    	return this.team.getColor();
     }
+}
+
+function GROUP(){
+
+}
+
+GROUP.prototype = {
+	initialize: function() {
+
+	}
+}
+
+function TEAM( id ){
+	this.id = id;
+	this.colorchart = ["#FF0000","#00FF00","#0000FF"];
+	this.color = this.colorchart[this.id];
+	console.log("the team color: " + this.color);
+}
+
+TEAM.prototype = {
+	initialize: function() {
+
+	},
+	getColor: function() {
+		return this.color;
+	}
 }
 
 
