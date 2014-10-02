ab2c62d9ae4e07932cd9ef891dbb61b195110c3f
Beau Bouchard <bnb2011@gmail.com>
2014-10-02 00:58:53 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,17 @@
 var game = new GAME();
 game.initialize();
 game.start();
+
+
+var a = document.getElementById("gamesvg");
+	a.addEventListener("load",function(){
+		//var svgDoc = a.contentDocument;
+  		var els = a.querySelectorAll("polygon");
+  		for (var i = 0, length = els.length; i < length; i++) {
+    		els[i].addEventListener("click", function(){ 
+
+    			console.log("column:" + this.getAttribute("column") + " row:" + this.getAttribute("row"));
+    			
+    			});
+    	}
+  	});
