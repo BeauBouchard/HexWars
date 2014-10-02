e6514dd84f60d5063af65938c757152b17070dd9
Beau Bouchard <bnb2011@gmail.com>
2014-10-02 00:59:08 -0400

    Update game.js

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -6,24 +6,26 @@
 
 
 function GAME() {
-	this.gamestate = 0; 
-	this.map;
-	this.players = [];
+    this.map;
+	this.gamestate   = 0; 
+	this.players     = [];
+    this.mapheight   = 600;
+    this.mapwidth    = 600;
+    this.regionCount = 20;
+    this.tilesize = Math.sqrt((this.mapwidth^2)+(this.mapheight^2))/(this.regionCount/5)
+    this.playerCount = 2;
 }
+
 GAME.prototype = {
     initialize: function() {
     	console.log("Initializing Game");
-    	this.map = new MAP();
-        this.hud = new HUD();
-    	this.map.initialize();
-        this.hud.initialize();
+    	this.map = new MAP(this.mapwidth,this.mapheight,this.tilesize,this.regionCount);
+    	this.map.generate();
+        //this.map.initCircle();
     },
     start: function() {
     	console.log("Starting Game");
-    	this.setupPlayers(2);
-    },
-    getGameState: function() {
-    	return this.gamestate;
+    	this.setupPlayers(this.playerCount);
     },
     restart: function() {
 
@@ -34,20 +36,14 @@ GAME.prototype = {
     		var player = new PLAYER(i);
     		this.players.push(player);
     	}
-    	this.setPlayerColor();
-    },
-    setPlayerColor: function() {
-    	//console.log(this.players.pop().getColor());
-    	//this.map.setTileColor(1,1,this.players[0].getColor());
-        //this.map.setTileFill(1,1,"#000032");
-        //this.map.setTileStroke(1,1,"#3333CC");
-
     }
 }
 
 function PLAYER(playernum) {
-	this.number = playernum;
-	this.team = new TEAM(this.number);
+	this.id = playernum;
+    this.colorchart = ['#f5f5f5', '#ffffff', '#dff0d8', '#d9edf7', '#fcf8e3', '#f2dede'];
+    this.color = this.colorchart[this.id];
+    console.log("Player" + this.id + "'s color is "+this.color );
 }
 
 PLAYER.prototype = {
@@ -55,51 +51,6 @@ PLAYER.prototype = {
 
     },
     getColor: function() {
-    	return this.team.getColor();
+    	return this.getColor();
     }
 }
-
-function GROUP(){
-
-}
-
-GROUP.prototype = {
-	initialize: function() {
-
-	}
-}
-
-function TEAM( id ){
-	this.id = id;
-	this.colorchart = ["#FF0000","#00FF00","#0000FF"];
-	this.color = this.colorchart[this.id];
-	console.log("the team color: " + this.color);
-}
-
-TEAM.prototype = {
-	initialize: function() {
-
-	},
-	getColor: function() {
-		return this.color;
-	}
-}
-
-function HUD (){
-    this.width = 300;
-    this.height = 200;
-}
-
-HUD.prototype = {
-    initialize: function() {
-
-    }
-}
-
-
-
-
-
-
-
-
